bb05ac002ffd2842d244ed89697d153c2b0cb421
choiseoji <choiseoji123@naver.com>
2025-11-05 20:17:30 +0900

    feat: code로 조회하는 코드 추가

--- src/main/java/kr/co/hdi/crawl/repository/ProductRepositoryCustom.java
+++ src/main/java/kr/co/hdi/crawl/repository/ProductRepositoryCustom.java
@@ -3,5 +3,9 @@ package kr.co.hdi.crawl.repository;
 import kr.co.hdi.crawl.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepositoryCustom extends JpaRepository<Product, Long>, kr.co.hdi.crawl.repository.query.ProductRepositoryCustom {
+
+    Optional<Product> findByOriginalId(Long originalId);
 }
--- src/main/java/kr/co/hdi/dataset/repository/BrandRepository.java
+++ src/main/java/kr/co/hdi/dataset/repository/BrandRepository.java
@@ -3,5 +3,9 @@ package kr.co.hdi.dataset.repository;
 import kr.co.hdi.dataset.domain.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+
+    Optional<Brand> findByBrandCode(String brandCode);
 }
