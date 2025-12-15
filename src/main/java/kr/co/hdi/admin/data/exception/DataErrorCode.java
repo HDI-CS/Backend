6ce3a806dec1a086b6b7d6ce3b590d15d3df9d41
choiseoji <choiseoji123@naver.com>
2025-12-16 03:24:36 +0900

    feat: 시각디자인 데이터 수정, 삭제 관련 API 추가

--- src/main/java/kr/co/hdi/admin/data/exception/DataErrorCode.java
+++ src/main/java/kr/co/hdi/admin/data/exception/DataErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum DataErrorCode implements ErrorCode {
 
-    DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 데이터를 찾을 수 없습니다.");
+    DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 데이터를 찾을 수 없습니다."),
+    YEAR_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 연도를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
