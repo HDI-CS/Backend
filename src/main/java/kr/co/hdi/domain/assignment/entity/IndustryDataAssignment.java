060d5173b499ce549b408a6c3b97edc4be60d2dc
choiseoji <choiseoji123@naver.com>
2025-12-21 16:18:18 +0900

    feat: 산업디자인 데이터 할당 코드 똑같이

--- src/main/java/kr/co/hdi/domain/assignment/entity/IndustryDataAssignment.java
+++ src/main/java/kr/co/hdi/domain/assignment/entity/IndustryDataAssignment.java
@@ -2,10 +2,14 @@ package kr.co.hdi.domain.assignment.entity;
 
 import jakarta.persistence.*;
 import kr.co.hdi.domain.data.entity.IndustryData;
+import kr.co.hdi.domain.data.entity.VisualData;
 import kr.co.hdi.domain.year.entity.UserYearRound;
 import kr.co.hdi.global.domain.BaseTimeEntityWithDeletion;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -25,4 +29,29 @@ public class IndustryDataAssignment extends BaseTimeEntityWithDeletion {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private IndustryData industryData;
+
+    @Builder
+    private IndustryDataAssignment(UserYearRound userYearRound, IndustryData industryData) {
+        this.userYearRound = userYearRound;
+        this.industryData = industryData;
+    }
+
+    public static IndustryDataAssignment create(
+            UserYearRound userYearRound,
+            IndustryData industryData
+    ) {
+        return IndustryDataAssignment.builder()
+                .userYearRound(userYearRound)
+                .industryData(industryData)
+                .build();
+    }
+
+    public static List<IndustryDataAssignment> createAll(
+            UserYearRound userYearRound,
+            List<IndustryData> industryDataList
+    ) {
+        return industryDataList.stream()
+                .map(industryData -> create(userYearRound, industryData))
+                .toList();
+    }
 }
--- src/main/java/kr/co/hdi/domain/assignment/repository/IndustryDataAssignmentRepository.java
+++ src/main/java/kr/co/hdi/domain/assignment/repository/IndustryDataAssignmentRepository.java
@@ -1,7 +1,36 @@
 package kr.co.hdi.domain.assignment.repository;
 
 import kr.co.hdi.domain.assignment.entity.IndustryDataAssignment;
+import kr.co.hdi.domain.assignment.entity.VisualDataAssignment;
+import kr.co.hdi.domain.year.entity.UserYearRound;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface IndustryDataAssignmentRepository extends JpaRepository<IndustryDataAssignment, Long> {
+import java.util.List;
+import java.util.Set;
+
+public interface IndustryDataAssignmentRepository extends JpaRepository<IndustryDataAssignment, Long>, IndustryDataAssignmentRepositoryCustom {
+
+    @Query("""
+        select ida
+        from IndustryDataAssignment ida
+        where ida.userYearRound = :userYearRound
+          and ida.deletedAt is null
+    """)
+    List<IndustryDataAssignment> findByUserYearRound(
+            @Param("userYearRound") UserYearRound userYearRound
+    );
+
+    @Modifying
+    @Query("""
+        DELETE FROM IndustryDataAssignment i
+        WHERE i.userYearRound = :userYearRound
+          AND i.industryData.id IN :industryDataIds
+    """)
+    void deleteByUserYearRoundAndIndustryDataIds(
+            @Param("userYearRound") UserYearRound userYearRound,
+            @Param("visualDataIds") Set<Long> industryDataIds
+    );
 }
